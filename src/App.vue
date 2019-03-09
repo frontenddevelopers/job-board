375b28c62e968060b2fbcfd034b9ea209048b846
Stefan Ivic <nightwalkerkg@gmail.com>
2019-03-09 21:12:11 +0100

    inital: basic scaffolding

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Job board</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat href="https://frontenddevelopers.org/" target="_blank">
+        <span class="mr-2">frontenddevelopers.org</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      //
+    }
+  }
+}
+</script>
